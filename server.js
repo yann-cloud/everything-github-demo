944f17712223d74527ba6f06787846531c6a333a
yann-cloud <obamayann9@gmail.com>
2022-04-21 13:34:52 +0200

    updates

--- server.js
+++ server.js
@@ -1,48 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-
-http.createServer(function (request, response) {
-
-   console.log('request starting for ');
-   console.log(request);
-
-   var filePath = '.' + request.url;
-   if (filePath == './')
-       filePath = './index.html';
-
-   console.log(filePath);
-   var extname = path.extname(filePath);
-   var contentType = 'text/html';
-   switch (extname) {
-       case '.js':
-           contentType = 'text/javascript';
-           break;
-       case '.css':
-           contentType = 'text/css';
-           break;
-   }
-
-   path.exists(filePath, function(exists) {
-
-       if (exists) {
-           fs.readFile(filePath, function(error, content) {
-               if (error) {
-                   response.writeHead(500);
-                   response.end();
-               }
-               else {
-                   response.writeHead(200, { 'Content-Type': contentType });
-                   response.end(content, 'utf-8');
-               }
-           });
-       }
-       else {
-           response.writeHead(404);
-           response.end();
-       }
-   });
-
-}).listen(process.env.PORT || 5000);
-
-console.log('Server running at http://127.0.0.1:5000/');
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/everything-github-demo'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/everything-github-demo/index.html') );});
+app.listen(process.env.PORT || 8080);
